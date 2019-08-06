208182a57a99a68f2aabead8d9ecb925769a8419
Jinzi Yan <stephanie.jinzi.yan@gmail.com>
2019-08-06 15:42:56 -0700

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  const num = parseInt(timeString.split('')[0]+timeString.split('')[1])
+  if(num < 12) {
+    return "Good Morning"
+  }
+  if(num > 17) {
+    return "Good Evening"
+  } else {
+    return "Good Afternoon"
+  }
+
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string
+}
